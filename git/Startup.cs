6efd7eac11e3a6517ed8bfd53031cb20db817bc6
kemalokko <40866183+kemalokko@users.noreply.github.com>
2018-07-05 20:44:00 +0300

    Add project files.

---
+++ git/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(git.Startup))]
+namespace git
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
